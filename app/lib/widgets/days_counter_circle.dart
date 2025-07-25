eb1647f086d2253598c80670435e4e20316988ac
J-shw <91017208+J-shw@users.noreply.github.com>
2025-07-25 23:11:05 +0100

    Made widget animated

--- app/lib/widgets/days_counter_circle.dart
+++ app/lib/widgets/days_counter_circle.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 
-// You'll likely want to put this in your components folder, e.g.,
-// lib/components/basic_progress_circle.dart
-class BasicProgressCircle extends StatelessWidget {
+class BasicProgressCircle extends StatefulWidget {
   final int currentValue;
   final int maxValue;
   final double circleSize;
   final double strokeWidth;
   final Color progressColor;
   final Color trackColor;
+  final Duration animationDuration; // New: Duration for the animation
 
   const BasicProgressCircle({
     super.key,
@@ -18,34 +17,82 @@ class BasicProgressCircle extends StatelessWidget {
     this.strokeWidth = 15.0,
     this.progressColor = Colors.red,
     this.trackColor = Colors.grey,
+    this.animationDuration = const Duration(milliseconds: 700),
   });
 
   @override
-  Widget build(BuildContext context) {
-    double progressValue = (1.0 - (currentValue / maxValue)).clamp(0.0, 1.0);
+  State<BasicProgressCircle> createState() => _BasicProgressCircleState();
+}
 
+class _BasicProgressCircleState extends State<BasicProgressCircle>
+    with SingleTickerProviderStateMixin {
+  
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  double _calculateTargetProgress() {
+    return (1.0 - (widget.currentValue / widget.maxValue)).clamp(0.0, 1.0);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.animationDuration,
+    );
+
+    _animation = Tween<double>(begin: 0.0, end: _calculateTargetProgress()).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      });
+
+    _controller.forward();
+  }
+
+  @override
+  void didUpdateWidget(covariant BasicProgressCircle oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.currentValue != oldWidget.currentValue || widget.maxValue != oldWidget.maxValue) {
+      _animation = Tween<double>(
+        begin: _animation.value, 
+        end: _calculateTargetProgress(), 
+      ).animate(_controller);
+      _controller.forward(from: 0.0);
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return SizedBox(
-      width: circleSize,
-      height: circleSize,
+      width: widget.circleSize,
+      height: widget.circleSize,
       child: Stack(
         alignment: Alignment.center,
         children: [
           SizedBox(
-            width: circleSize,
-            height: circleSize,
+            width: widget.circleSize,
+            height: widget.circleSize,
             child: CircularProgressIndicator(
               year2023: false,
-              value: progressValue,
-              strokeWidth: strokeWidth,
-              valueColor: AlwaysStoppedAnimation<Color>(progressColor),
-              backgroundColor: trackColor,
+              value: _animation.value,
+              strokeWidth: widget.strokeWidth,
+              valueColor: AlwaysStoppedAnimation<Color>(widget.progressColor),
+              backgroundColor: widget.trackColor,
             ),
           ),
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                '$currentValue',
+                '${widget.currentValue}',
                 style: const TextStyle(
                   fontSize: 60,
                   fontWeight: FontWeight.bold,
