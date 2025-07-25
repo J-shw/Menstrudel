5ae7709502769b727187e98acfdf8cb1f5b6d440
J-shw <91017208+J-shw@users.noreply.github.com>
2025-07-26 00:20:10 +0100

    Update main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:menstrudel/widgets/days_counter_circle.dart';
+import 'package:menstrudel/widgets/basic_progress_circle.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,47 +9,54 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-		theme: ThemeData(
-			useMaterial3: true,
-			// Define your light color scheme
-			colorScheme: ColorScheme.fromSeed(
-				seedColor: Colors.blue, // A vibrant red for light mode progress
-				brightness: Brightness.light,
+	@override
+	Widget build(BuildContext context) {
+		return MaterialApp(
+			theme: ThemeData(
+				useMaterial3: true,
+				// Define your light color scheme
+				colorScheme: ColorScheme.fromSeed(
+					seedColor: Colors.blue, // A vibrant red for light mode progress
+					brightness: Brightness.light,
+				),
+				appBarTheme: const AppBarTheme(
+					backgroundColor: Colors.red, // Example app bar color for light theme
+					foregroundColor: Colors.white, // Text color for app bar
+				),
 			),
-			appBarTheme: const AppBarTheme(
-				backgroundColor: Colors.red, // Example app bar color for light theme
-				foregroundColor: Colors.white, // Text color for app bar
+			darkTheme: ThemeData(
+				useMaterial3: true,
+				// Define your dark color scheme
+				colorScheme: ColorScheme.fromSeed(
+					seedColor: Colors.blue, // Using the same seed color helps maintain consistency
+					brightness: Brightness.dark,
+				),
+				appBarTheme: AppBarTheme(
+					backgroundColor: Colors.red[900], // Darker red for dark mode app bar
+					foregroundColor: Colors.white,
+				),
 			),
-		),
-      	darkTheme: ThemeData(
-			useMaterial3: true,
-			// Define your dark color scheme
-			colorScheme: ColorScheme.fromSeed(
-				seedColor: Colors.blue, // Using the same seed color helps maintain consistency
-				brightness: Brightness.dark,
+			themeMode: ThemeMode.system, 
+			home: Scaffold(
+				// backgroundColor: Colors.white,
+				body: Center(
+					child: BasicProgressCircle(
+						currentValue: 7, // Example: 7 days left
+						maxValue: 30,  // Out of 30 days total
+						circleSize: 220,
+						strokeWidth: 20,
+						progressColor: Color.fromARGB(255, 255, 118, 118),
+						trackColor: Color.fromARGB(20, 255, 118, 118),
+					),
+				),
+				floatingActionButton: FloatingActionButton(
+					onPressed:
+						() => true,
+					tooltip: 'Log symptoms',
+					child: const Icon(Icons.add),
+				),
+				floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
 			),
-			appBarTheme: AppBarTheme(
-				backgroundColor: Colors.red[900], // Darker red for dark mode app bar
-				foregroundColor: Colors.white,
-			),
-		),
-		themeMode: ThemeMode.system, 
-		home: Scaffold(
-			// backgroundColor: Colors.white,
-			body: const Center(
-			child: BasicProgressCircle(
-				currentValue: 7, // Example: 7 days left
-				maxValue: 30,  // Out of 30 days total
-				circleSize: 220,
-				strokeWidth: 20,
-				progressColor: Color.fromARGB(255, 255, 118, 118),
-				trackColor: Color.fromARGB(20, 255, 118, 118),
-			),
-			),
-		),
 		);
 	}
 }
