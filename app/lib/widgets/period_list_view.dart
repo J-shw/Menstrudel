a53021d72881a19b6a1e9b06a4c634df67943cd5
J-shw <91017208+J-shw@users.noreply.github.com>
2025-07-26 14:18:41 +0100

    The foundations

--- app/lib/widgets/period_list_view.dart
+++ app/lib/widgets/period_list_view.dart
@@ -7,11 +7,13 @@ final List<String> flowLabels = ["Low", "Medium", "High"];
 class PeriodListView extends StatelessWidget {
 	final List<PeriodEntry> periodEntries;
 	final bool isLoading;
+	final Function(int) onDelete;
 
 	const PeriodListView({
 		super.key,
 		required this.periodEntries,
 		required this.isLoading,
+		required this.onDelete,
 	});
 
 	@override
@@ -29,20 +31,57 @@ class PeriodListView extends StatelessWidget {
 			} else {
 			return Expanded(
 				child: ListView.builder(
-				itemCount: periodEntries.length,
-				itemBuilder: (context, index) {
-					final entry = periodEntries[index];
-					final String displayedSymptom = entry.symptom?.isNotEmpty == true ? entry.symptom! : 'No specific symptom';
-					return Card(
-					margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-					child: ListTile(
-						title: Text(DateFormat('dd/MM/yyyy').format(entry.date), style: TextStyle(fontWeight: FontWeight.bold)),
-						subtitle: Text(
-							'Symptons: $displayedSymptom | Flow: ${flowLabels[entry.flow]}',
-						),
-					),
-					);
-				},
+					itemCount: periodEntries.length,
+					itemBuilder: (context, index) {
+						final entry = periodEntries[index];
+						final String displayedSymptom = entry.symptom?.isNotEmpty == true ? entry.symptom! : 'No specific symptom';
+						return Dismissible(
+							key: ValueKey(entry.id),
+							direction: DismissDirection.endToStart,
+							background: Container(
+								color: Colors.red,
+								alignment: Alignment.centerRight,
+								padding: const EdgeInsets.symmetric(horizontal: 20),
+								child: const Icon(Icons.delete, color: Colors.white),
+							),
+							confirmDismiss: (direction) async {
+								return await showDialog(
+									context: context,
+									builder: (BuildContext context) {
+										return AlertDialog(
+										title: const Text("Confirm Delete"),
+										content: const Text("Are you sure you want to delete this entry?"),
+										actions: <Widget>[
+											TextButton(
+											onPressed: () => Navigator.of(context).pop(false), // Don't dismiss
+											child: const Text("Cancel"),
+											),
+											ElevatedButton(
+											onPressed: () => Navigator.of(context).pop(true), // Confirm dismissal
+											child: const Text("Delete"),
+											),
+										],
+										);
+									},
+								);
+							},
+							onDismissed: (direction) {
+								print(entry.id);
+								if (entry.id != null) {
+									onDelete(entry.id!);
+								}
+							},
+							child: Card(
+								margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+								child: ListTile(
+									title: Text(DateFormat('dd/MM/yyyy').format(entry.date), style: TextStyle(fontWeight: FontWeight.bold)),
+									subtitle: Text(
+										'Symptons: $displayedSymptom | Flow: ${flowLabels[entry.flow]}',
+									),
+								),
+							),
+						);
+					},
 				),
 			);
 		}
