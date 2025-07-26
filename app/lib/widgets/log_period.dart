94f2e834468c279f01eb839609ee4e2a1971cbad
J-shw <91017208+J-shw@users.noreply.github.com>
2025-07-26 08:42:16 +0100

    Update log_period.dart

--- app/lib/widgets/log_period.dart
+++ app/lib/widgets/log_period.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart'; // Import for date formatting
+import 'package:intl/intl.dart';
 
 class SymptomEntryDialog extends StatefulWidget {
 const SymptomEntryDialog({super.key});
@@ -9,10 +9,9 @@ State<SymptomEntryDialog> createState() => _SymptomEntryDialogState();
 }
 
 class _SymptomEntryDialogState extends State<SymptomEntryDialog> {
-DateTime _selectedDate = DateTime.now(); // Default to today's date
-String? _selectedSymptom; // Nullable for no selection initially
+DateTime _selectedDate = DateTime.now();
+String? _selectedSymptom;
 
-// List of symptom options for the dropdown
 final List<String> _symptomOptions = [
 	'Headache',
 	'Fatigue',
@@ -29,90 +28,89 @@ double _currentDiscreteSliderValue = 1;
 
 @override
 Widget build(BuildContext context) {
-	// Access the current theme's color scheme for adaptive colors
 	final ColorScheme colorScheme = Theme.of(context).colorScheme;
 
 	return AlertDialog(
 		title: const Text('Log period'),
 		content: SingleChildScrollView(
 			child: Column(
-			mainAxisSize: MainAxisSize.min,
-			children: [
-				ListTile(
-					leading: Icon(Icons.calendar_today, color: colorScheme.onSurfaceVariant),
-					title: Text(
-						'Select Date:',
-						style: TextStyle(color: colorScheme.onSurface),
-					),
-					subtitle: Text(
-						DateFormat('dd/MM/yyyy').format(_selectedDate),
-						style: TextStyle(
-						color: colorScheme.onSurfaceVariant,
-						fontWeight: FontWeight.bold,
+				mainAxisSize: MainAxisSize.min,
+				children: [
+					ListTile(
+						leading: Icon(Icons.calendar_today, color: colorScheme.onSurfaceVariant),
+						title: Text(
+							'Select Date:',
+							style: TextStyle(color: colorScheme.onSurface),
 						),
-					),
-					onTap: () async {
-						final DateTime? picked = await showDatePicker(
-						context: context,
-						initialDate: _selectedDate,
-						firstDate: DateTime(2000),
-						lastDate: DateTime.now(),
-						builder: (context, child) {
-							return Theme(
-							data: Theme.of(context).copyWith(
-								colorScheme: colorScheme,
+						subtitle: Text(
+							DateFormat('dd/MM/yyyy').format(_selectedDate),
+							style: TextStyle(
+								color: colorScheme.onSurfaceVariant,
+								fontWeight: FontWeight.bold,
 							),
-							child: child!,
+						),
+						onTap: () async {
+							final DateTime? picked = await showDatePicker(
+							context: context,
+							initialDate: _selectedDate,
+							firstDate: DateTime(2000),
+							lastDate: DateTime.now(),
+							builder: (context, child) {
+								return Theme(
+								data: Theme.of(context).copyWith(
+									colorScheme: colorScheme,
+								),
+								child: child!,
+								);
+							},
 							);
+							if (picked != null && picked != _selectedDate) {
+							setState(() {
+								_selectedDate = picked;
+							});
+							}
 						},
-						);
-						if (picked != null && picked != _selectedDate) {
-						setState(() {
-							_selectedDate = picked;
-						});
-						}
-					},
-				),
-				const SizedBox(height: 15),
-				Slider(
-					year2023: false,
-					value: _currentDiscreteSliderValue,
-					max: 2,
-					divisions: 2,
-					label: flowLabels[_currentDiscreteSliderValue.toInt()],
-					onChanged: (double value) {
-						setState(() {
-						_currentDiscreteSliderValue = value;
-						});
-					},
-				),
-
-				const SizedBox(height: 15),
-
-				DropdownButtonFormField<String>(
-					decoration: InputDecoration(
-						labelText: 'Select Symptom',
-						border: const OutlineInputBorder(),
-						labelStyle: TextStyle(color: colorScheme.onSurface),
-						hintStyle: TextStyle(color: colorScheme.onSurfaceVariant),
 					),
-					value: _selectedSymptom,
-					hint: const Text('Choose a symptom'),
-					items: _symptomOptions.map((String symptom) {
-						return DropdownMenuItem<String>(
-						value: symptom,
-						child: Text(symptom, style: TextStyle(color: colorScheme.onSurface)),
-						);
-					}).toList(),
-					onChanged: (String? newValue) {
-						setState(() {
-						_selectedSymptom = newValue;
-					});
-				},
-				dropdownColor: colorScheme.surface,
-				style: TextStyle(color: colorScheme.onSurface),
-				),
-			],
+					const SizedBox(height: 15),
+					Slider(
+						year2023: false,
+						value: _currentDiscreteSliderValue,
+						max: 2,
+						divisions: 2,
+						label: flowLabels[_currentDiscreteSliderValue.toInt()],
+						onChanged: (double value) {
+							setState(() {
+							_currentDiscreteSliderValue = value;
+							});
+						},
+					),
+
+					const SizedBox(height: 15),
+
+					DropdownButtonFormField<String>(
+						decoration: InputDecoration(
+							labelText: 'Select Symptom',
+							border: const OutlineInputBorder(),
+							labelStyle: TextStyle(color: colorScheme.onSurface),
+							hintStyle: TextStyle(color: colorScheme.onSurfaceVariant),
+						),
+						value: _selectedSymptom,
+						hint: const Text('Choose a symptom'),
+						items: _symptomOptions.map((String symptom) {
+							return DropdownMenuItem<String>(
+							value: symptom,
+							child: Text(symptom, style: TextStyle(color: colorScheme.onSurface)),
+							);
+						}).toList(),
+						onChanged: (String? newValue) {
+							setState(() {
+							_selectedSymptom = newValue;
+						});
+					},
+					dropdownColor: colorScheme.surface,
+					style: TextStyle(color: colorScheme.onSurface),
+					),
+				],
 			),
 		),
 		actions: <Widget>[
