cd355492332747906160583ab9e1eb654cdbdc64
J-shw <91017208+J-shw@users.noreply.github.com>
2025-07-29 19:06:19 +0100

    Added total_days

--- app/lib/models/period.dart
+++ app/lib/models/period.dart
@@ -2,11 +2,13 @@ class PeriodEntry {
 	final int? id;
 	final DateTime startDate;
 	final DateTime endDate;
+	final int totalDays;
 
 	PeriodEntry({
 		this.id, 
 		required this.startDate, 
-		required this.endDate
+		required this.endDate,
+		required this.totalDays
 	});
 
 	Map<String, dynamic> toMap() {
@@ -14,6 +16,7 @@ class PeriodEntry {
 			'id': id,
 			'start_date': startDate.millisecondsSinceEpoch,
 			'end_date': endDate.millisecondsSinceEpoch,
+			'total_days': totalDays,
 		};
 	}
 
@@ -22,6 +25,7 @@ class PeriodEntry {
 			id: map['id'],
 			startDate: DateTime.fromMillisecondsSinceEpoch(map['start_date']),
 			endDate: DateTime.fromMillisecondsSinceEpoch(map['end_date']),
+			totalDays: map['total_days'],
 		);
 	}
 
@@ -29,11 +33,13 @@ class PeriodEntry {
 		int? id,
 		DateTime? startDate,
 		DateTime? endDate,
+		int? totalDays,
 	}) {
 		return PeriodEntry(
 			id: id ?? this.id,
 			startDate: startDate ?? this.startDate,
 			endDate: endDate ?? this.endDate,
+			totalDays: totalDays ?? this.totalDays,
 		);
 	}
 }
