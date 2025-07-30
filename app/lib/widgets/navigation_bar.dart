a7236cbdd44c5ba32152ff2c8b40a8ba3067b71d
J-shw <91017208+J-shw@users.noreply.github.com>
2025-07-30 18:26:38 +0100

    Improved UI + added isActive checks for each page.
    The isActive check will disable the button so that users cannot access the current screen over and over again.

--- app/lib/widgets/navigation_bar.dart
+++ app/lib/widgets/navigation_bar.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:menstrudel/screens/analytics_screen.dart';
+import 'package:menstrudel/screens/home_screen.dart';
+
 
 class MainBottomNavigationBar extends StatelessWidget {
+  final bool isAnalyticsScreenActive;
+  final bool isHomeScreenActive;
   
   const MainBottomNavigationBar({
     super.key,
+    this.isAnalyticsScreenActive = false,
+    this.isHomeScreenActive = false,
   });
 
   @override
@@ -15,35 +21,39 @@ class MainBottomNavigationBar extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.bar_chart, size: 30.0),
-                  tooltip: 'Logs',
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => AnalyticsScreen(),
-                      ),
-                    );
-                  },
-                ),	
-              ],
+            IconButton(
+              icon: const Icon(Icons.bar_chart, size: 30.0),
+              tooltip: 'Analytics',
+              onPressed: isAnalyticsScreenActive
+              ? null
+              : () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const AnalyticsScreen(),
+                  ),
+                );
+              },
             ),
-            const SizedBox(width: 48.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                // IconButton(
-                // 	icon: const Icon(Icons.settings, size: 30.0),
-                // 	tooltip: 'Settings',
-                // 	onPressed: () {
-                    
-                // 	},
-                // ),	
-              ],
+            IconButton(
+              icon: const Icon(Icons.home, size: 30.0),
+              tooltip: 'Home',
+              onPressed: isHomeScreenActive
+              ? null
+              : () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const HomeScreen(),
+                  ),
+                );
+              },
             ),
+            IconButton(
+              icon: const Icon(Icons.settings, size: 30.0),
+              tooltip: 'Settings',
+              onPressed: () {
+                
+              },
+            ),	
           ],
         )
       ),
