4c5226265d7b87df0fe0196ba4a13a8acde4d6ea
J-shw <91017208+J-shw@users.noreply.github.com>
2025-07-31 20:41:22 +0100

    Added notification initialisation

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:menstrudel/screens/home_screen.dart';
+import 'package:menstrudel/services/period_notifications.dart';
 
-void main() {
+void main() async { 
+  WidgetsFlutterBinding.ensureInitialized();
+  await NotificationHelper.initialiseNotifications(); 
   runApp(const MainApp());
 }
 
