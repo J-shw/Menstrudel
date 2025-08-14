21357800ea946968b7cc6389dfd94bf00766eade
Josh <91017208+J-shw@users.noreply.github.com>
2025-08-14 11:44:22 +0100

    Added FAB support for homescreen

--- app/lib/screens/main_screen.dart
+++ app/lib/screens/main_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:menstrudel/screens/analytics_screen.dart';
 import 'package:menstrudel/screens/home_screen.dart';
 import 'package:menstrudel/screens/settings_screen.dart';
-import 'package:menstrudel/widgets/main/navigation_bar.dart';
+import 'package:menstrudel/widgets/main/main_navigation_bar.dart';
 import 'package:menstrudel/widgets/main/app_bar.dart';
 
 class MainScreen extends StatefulWidget {
@@ -15,17 +15,37 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 1;
 
-  static const List<Widget> _pages = <Widget>[
-    AnalyticsScreen(),
-    HomeScreen(),
-    SettingsScreen(),
-  ];
+  final GlobalKey<HomeScreenState> _homeScreenKey = GlobalKey<HomeScreenState>();
   
+  bool _isPeriodOngoing = false;
+
+  late final List<Widget> _pages;
   static const List<PreferredSizeWidget?> _appBars = <PreferredSizeWidget?>[
-    TopAppBar(titleText: "Analytics"),
+    TopAppBar(titleText: "Insights"),
     null,
     TopAppBar(titleText: "Settings"),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    _pages = <Widget>[
+      const AnalyticsScreen(),
+      HomeScreen(
+        key: _homeScreenKey,
+        onPeriodStatusChange: _onPeriodStatusChange,
+      ),
+      const SettingsScreen(),
+    ];
+  }
+
+  void _onPeriodStatusChange(bool isOngoing) {
+    if (_isPeriodOngoing != isOngoing) {
+      setState(() {
+        _isPeriodOngoing = isOngoing;
+      });
+    }
+  }
 
   void _onItemTapped(int index) {
     setState(() {
@@ -37,11 +57,37 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBars[_selectedIndex],
+
       body: _pages[_selectedIndex],
+
       bottomNavigationBar: MainNavigationBar(
         selectedIndex: _selectedIndex,
         onScreenSelected: _onItemTapped,
       ),
+
+      floatingActionButton: _selectedIndex == 1
+        ? Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Visibility(
+                visible: _isPeriodOngoing,
+                child: FloatingActionButton(
+                  tooltip: 'Tampon reminder',
+                  onPressed: () => _homeScreenKey.currentState?.handleTamponReminder(context),
+                  child: const Icon(Icons.add_alarm),
+                ),
+              ),
+              Visibility(
+                visible: !_isPeriodOngoing,
+                child: FloatingActionButton(
+                  tooltip: 'Log period',
+                  onPressed: () => _homeScreenKey.currentState?.handleLogPeriod(context),
+                  child: const Icon(Icons.add),
+                ),
+              ),
+            ],
+          )
+        : null,
     );
   }
 }
