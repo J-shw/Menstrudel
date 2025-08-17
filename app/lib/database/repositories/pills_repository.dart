4bb3b9d74032f1ff2964f37a05e7a9c907fc06bc
J-shw <91017208+J-shw@users.noreply.github.com>
2025-08-17 01:25:27 +0100

    Create pills_repository.dart

---
+++ app/lib/database/repositories/pills_repository.dart
@@ -0,0 +1,73 @@
+import 'package:menstrudel/database/app_database.dart';
+import 'package:menstrudel/models/pills/pill_regimen.dart';
+import 'package:menstrudel/models/pills/pill_intake.dart';
+import 'package:menstrudel/models/pills/pill_reminder.dart';
+
+class PillsRepository {
+  final dbProvider = AppDatabase.instance;
+
+  Future<PillIntake> createPillIntake(PillIntake intake) async {
+    final db = await dbProvider.database;
+    final id = await db.insert('PillIntake', intake.toMap());
+    return intake.copyWith(id: id);
+  }
+
+  Future<PillRegimen?> readActivePillRegimen() async {
+    final db = await dbProvider.database;
+    final maps = await db.query(
+      'PillRegimen',
+      where: 'is_active = ?',
+      whereArgs: [1],
+      limit: 1,
+    );
+
+    if (maps.isNotEmpty) {
+      return PillRegimen.fromMap(maps.first);
+    }
+    return null;
+  }
+
+  Future<List<PillIntake>> readIntakesForRegimen(int regimenId) async {
+    final db = await dbProvider.database;
+    final result = await db.query(
+      'PillIntake',
+      where: 'regimen_id = ?',
+      whereArgs: [regimenId],
+    );
+    return result.map((json) => PillIntake.fromMap(json)).toList();
+  }
+
+  Future<PillReminder?> readReminderForRegimen(int regimenId) async {
+    final db = await dbProvider.database;
+    final maps = await db.query(
+      'PillReminder',
+      where: 'regimen_id = ?',
+      whereArgs: [regimenId],
+      limit: 1,
+    );
+
+    if (maps.isNotEmpty) {
+      return PillReminder.fromMap(maps.first);
+    }
+    return null;
+  }
+
+  Future<PillRegimen> createPillRegimen(PillRegimen regimen) async {
+    final db = await dbProvider.database;
+    await db.transaction((txn) async {
+      await txn.update('PillRegimen', {'is_active': 0}, where: 'is_active = 1');
+      final id = await txn.insert('PillRegimen', regimen.toMap());
+      return regimen.copyWith(id: id);
+    });
+    return regimen;
+  }
+
+  Future<void> deletePillRegimen(int id) async {
+    final db = await dbProvider.database;
+    await db.delete(
+      'PillRegimen',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
