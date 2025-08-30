0b2718bdbd10fc9a235241624a7aac71b7ad76c4
J-shw <91017208+J-shw@users.noreply.github.com>
2025-08-30 20:33:07 +0100

    catches duplicate period and alerts user

--- app/lib/services/period_logger_service.dart
+++ app/lib/services/period_logger_service.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:menstrudel/widgets/sheets/symptom_entry_sheet.dart';
 import 'package:menstrudel/models/period_logs/period_logs.dart';
 import 'package:menstrudel/database/repositories/periods_repository.dart';
+import 'package:menstrudel/utils/exceptions.dart';
 
 class PeriodLoggerService {
   static Future<bool> showAndLogPeriod(BuildContext context, DateTime selectedDate) async {
@@ -31,7 +32,14 @@ class PeriodLoggerService {
         flow: result['flow'] ?? 0,
       );
 
-      await periodsRepo.createPeriodLog(newEntry); 
+      try {
+        await periodsRepo.createPeriodLog(newEntry);
+      } on DuplicateLogException catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(e.message)),
+        );
+        return false;
+      }
 
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Log saved!')),
