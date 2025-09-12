3aef203317bcf5c40c87c3cb394989ea87b5482e
J-shw <91017208+J-shw@users.noreply.github.com>
2025-09-12 21:36:40 +0200

    Now sends data instead of saving

--- app/lib/services/wear_sync_service.dart
+++ app/lib/services/wear_sync_service.dart
@@ -1,22 +1,12 @@
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:watch_connectivity/watch_connectivity.dart';
 
-class WearPredictionData {
-  final int daysUntilDue;
+class WatchSyncService {
+  final _watch = WatchConnectivity();
 
-  WearPredictionData({required this.daysUntilDue});
-}
-
-class WearSyncService {
-  static const _keyDaysUntilDue = 'wear_daysUntilDue';
-
-  Future<void> savePrediction({required int daysUntilDue}) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt(_keyDaysUntilDue, daysUntilDue);
-  }
-
-  Future<WearPredictionData> readPrediction() async {
-    final prefs = await SharedPreferences.getInstance();
-    final days = prefs.getInt(_keyDaysUntilDue) ?? -1;
-    return WearPredictionData(daysUntilDue: days);
+  Future<void> sendPrediction({required int daysUntilDue}) async {
+    final data = <String, dynamic>{
+      'daysUntilDue': daysUntilDue,
+    };
+    _watch.sendMessage(data);
   }
 }
