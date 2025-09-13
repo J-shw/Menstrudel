2b98d03831f0b98dd0189a3c89e64219a3aecaa4
J-shw <91017208+J-shw@users.noreply.github.com>
2025-09-13 21:09:07 +0100

    Now obtains watch period log attempts from app context

--- app/lib/services/wear_sync_service.dart
+++ app/lib/services/wear_sync_service.dart
@@ -1,7 +1,34 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:watch_connectivity/watch_connectivity.dart';
 
 class WatchSyncService {
   final _watch = WatchConnectivity();
+  StreamSubscription? _subscription;
+
+  void Function()? onPeriodLogRequested;
+
+  Future<void> initialize({required void Function() onPeriodLog}) async {
+    onPeriodLogRequested = onPeriodLog;
+
+    debugPrint('Initialising and listening on applicationContextStream...');
+
+
+    final missedContexts = await _watch.receivedApplicationContexts;
+    for (final context in missedContexts) {
+      _handleContext(context);
+    }
+
+    _subscription = _watch.contextStream.listen(_handleContext);
+  }
+  
+  void _handleContext(Map<String, dynamic> context) {
+    debugPrint('Processing context: $context');
+    if (context['log_period'] == true) {
+      onPeriodLogRequested?.call();
+    }
+  }
 
   Future<void> sendCircleData({
       required int circleMaxValue,
@@ -12,5 +39,9 @@ class WatchSyncService {
       'circleCurrentValue': circleCurrentValue,
     };
     _watch.sendMessage(data);
+  }
+
+  void dispose() {
+    _subscription?.cancel();
   }
 }
