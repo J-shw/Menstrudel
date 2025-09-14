9f73fe737bf1717820e2955b037c2b8c3a12ec20
J-shw <91017208+J-shw@users.noreply.github.com>
2025-09-14 20:11:25 +0100

    Added getThemeMode method

--- app/lib/models/themes/app_theme_mode_enum.dart
+++ app/lib/models/themes/app_theme_mode_enum.dart
@@ -1,4 +1,5 @@
 import 'package:menstrudel/l10n/app_localizations.dart';
+import 'package:flutter/material.dart';
 
 enum AppThemeMode { light, dark, system }
 
@@ -12,6 +13,17 @@ extension AppthememodeExtension on AppThemeMode {
         return l10n.settingsScreen_themeDark;
       case AppThemeMode.system:
         return l10n.settingsScreen_themeSystem;
+    }
+  }
+
+  ThemeMode getThemeMode() {
+    switch (this) {
+      case AppThemeMode.light:
+        return ThemeMode.light;
+      case AppThemeMode.dark:
+        return ThemeMode.dark;
+      case AppThemeMode.system:
+        return ThemeMode.system;
     }
   }
 }
