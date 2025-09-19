6623bf2e999e44ab6737a2c38da7303e2621c440
Josh <91017208+J-shw@users.noreply.github.com>
2025-09-19 14:29:57 +0100

    Added spotting

--- app/lib/models/flows/flow_enum.dart
+++ app/lib/models/flows/flow_enum.dart
@@ -3,6 +3,7 @@ import 'package:menstrudel/l10n/app_localizations.dart';
 
 enum FlowRate {
   none,
+  spotting,
   light,
   medium,
   heavy,
@@ -13,6 +14,8 @@ extension FlowExtension on FlowRate {
     switch (this) {
       case FlowRate.none:
         return l10n.flowIntensity_none;
+      case FlowRate.spotting:
+        return l10n.flowIntensity_spotting;
       case FlowRate.light:
         return l10n.flowIntensity_light;
       case FlowRate.medium:
@@ -28,12 +31,14 @@ extension FlowExtension on FlowRate {
     switch (this) {
       case FlowRate.none:
         return Colors.blue.shade300;
+      case FlowRate.spotting:
+        return Colors.pink.shade100;
       case FlowRate.light:
         return Colors.pink.shade200;
       case FlowRate.medium:
         return Colors.pink.shade400;
       case FlowRate.heavy:
-        return Colors.red.shade700;
+        return Colors.red.shade600;
     }
   }
 }
