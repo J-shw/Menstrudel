c6e706655e0aa7dbfd91dde37478bfa763ca52d8
J-shw <91017208+J-shw@users.noreply.github.com>
2025-10-05 18:26:04 -0600

    Uses constants for keys

--- app/lib/services/settings_service.dart
+++ app/lib/services/settings_service.dart
@@ -2,23 +2,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:menstrudel/utils/constants.dart';
 import 'package:menstrudel/models/themes/app_theme_mode_enum.dart';
+import 'package:menstrudel/utils/constants.dart';
 
 enum PeriodHistoryView { list, journal }
 
 class SettingsService {
-  static const String _notificationsEnabledKey = 'notifications_enabled';
-  static const String _notificationDaysKey = 'notification_days';
-  static const _notificationTimeKey = 'notification_time';
-  static const String _periodOverdueNotificationsEnabledKey = 'period_overdue_notifications_enabled';
-  static const String _periodOverdueNotificationDaysKey = 'period_overdue_notification_days';
-  static const _periodOverdueNotificationTimeKey = 'period_overdue_notification_time';
-  static const _biometricEnabledKey = 'biometric_enabled';
-  static const _historyViewKey = 'history_view';
-  static const _dynamicColorKey = 'dynamic_color';
-  static const _themeColorKey = 'theme_color';
-  static const _themeModeKey = 'theme_mode';
-  static const _persistentReminderKey = "always_show_reminder_button";
-
   Future<void> deleteAllSettings() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.clear();
@@ -26,53 +14,53 @@ class SettingsService {
 
   Future<void> setAlwaysShowReminderButtonEnabled(bool isEnabled) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(_persistentReminderKey, isEnabled);
+    await prefs.setBool(persistentReminderKey, isEnabled);
   }
 
   Future<bool> areAlwaysShowReminderButtonEnabled() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool(_persistentReminderKey) ?? false;
+    return prefs.getBool(persistentReminderKey) ?? false;
   }
 
   Future<void> setBiometricsEnabled(bool isEnabled) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(_biometricEnabledKey, isEnabled);
+    await prefs.setBool(biometricEnabledKey, isEnabled);
   }
 
   Future<bool> areBiometricsEnabled() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool(_biometricEnabledKey) ?? false;
+    return prefs.getBool(biometricEnabledKey) ?? false;
   }
 
   Future<void> setNotificationsEnabled(bool isEnabled) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(_notificationsEnabledKey, isEnabled);
+    await prefs.setBool(notificationsEnabledKey, isEnabled);
   }
 
   Future<bool> areNotificationsEnabled() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool(_notificationsEnabledKey) ?? true;
+    return prefs.getBool(notificationsEnabledKey) ?? true;
   }
 
   Future<void> setNotificationDays(int days) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt(_notificationDaysKey, days);
+    await prefs.setInt(notificationDaysKey, days);
   }
 
   Future<int> getNotificationDays() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getInt(_notificationDaysKey) ?? 1;
+    return prefs.getInt(notificationDaysKey) ?? 1;
   }
 
   Future<void> setNotificationTime(TimeOfDay time) async {
     final prefs = await SharedPreferences.getInstance();
     final String formattedTime = '${time.hour}:${time.minute}';
-    await prefs.setString(_notificationTimeKey, formattedTime);
+    await prefs.setString(notificationTimeKey, formattedTime);
   }
 
   Future<TimeOfDay> getNotificationTime() async {
     final prefs = await SharedPreferences.getInstance();
-    final String? storedTime = prefs.getString(_notificationTimeKey);
+    final String? storedTime = prefs.getString(notificationTimeKey);
 
     if (storedTime == null) {
       return const TimeOfDay(hour: 9, minute: 0);
@@ -89,33 +77,33 @@ class SettingsService {
 
   Future<void> setPeriodOverdueNotificationsEnabled(bool isEnabled) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(_periodOverdueNotificationsEnabledKey, isEnabled);
+    await prefs.setBool(periodOverdueNotificationsEnabledKey, isEnabled);
   }
 
   Future<bool> arePeriodOverdueNotificationsEnabled() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool(_periodOverdueNotificationsEnabledKey) ?? true;
+    return prefs.getBool(periodOverdueNotificationsEnabledKey) ?? true;
   }
 
   Future<void> setPeriodOverdueNotificationDays(int days) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt(_periodOverdueNotificationDaysKey, days);
+    await prefs.setInt(periodOverdueNotificationDaysKey, days);
   }
 
   Future<int> getPeriodOverdueNotificationDays() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getInt(_periodOverdueNotificationDaysKey) ?? 1;
+    return prefs.getInt(periodOverdueNotificationDaysKey) ?? 1;
   }
 
   Future<void> setPeriodOverdueNotificationTime(TimeOfDay time) async {
     final prefs = await SharedPreferences.getInstance();
     final String formattedTime = '${time.hour}:${time.minute}';
-    await prefs.setString(_periodOverdueNotificationTimeKey, formattedTime);
+    await prefs.setString(periodOverdueNotificationTimeKey, formattedTime);
   }
 
   Future<TimeOfDay> getPeriodOverdueNotificationTime() async {
     final prefs = await SharedPreferences.getInstance();
-    final String? storedTime = prefs.getString(_periodOverdueNotificationTimeKey);
+    final String? storedTime = prefs.getString(periodOverdueNotificationTimeKey);
 
     if (storedTime == null) {
       return const TimeOfDay(hour: 9, minute: 0);
@@ -130,12 +118,12 @@ class SettingsService {
 
   Future<void> setHistoryView(PeriodHistoryView view) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(_historyViewKey, view.name);
+    await prefs.setString(historyViewKey, view.name);
   }
 
   Future<PeriodHistoryView> getHistoryView() async {
     final prefs = await SharedPreferences.getInstance();
-    final viewName = prefs.getString(_historyViewKey);
+    final viewName = prefs.getString(historyViewKey);
     return PeriodHistoryView.values.firstWhere(
       (e) => e.name == viewName,
       orElse: () => PeriodHistoryView.journal,
@@ -144,31 +132,31 @@ class SettingsService {
 
   Future<void> setDynamicColorEnabled(bool isEnabled) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(_dynamicColorKey, isEnabled);
+    await prefs.setBool(dynamicColorKey, isEnabled);
   }
   Future<bool> isDynamicThemeEnabled() async {
     final prefs = await SharedPreferences.getInstance();
-    final isEnabled = prefs.getBool(_dynamicColorKey) ?? false;
+    final isEnabled = prefs.getBool(dynamicColorKey) ?? false;
     return isEnabled;
   }
 
   Future<void> setThemeColor(Color color) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt(_themeColorKey, color.toARGB32());
+    await prefs.setInt(themeColorKey, color.toARGB32());
   }
   Future<Color> getThemeColor() async {
     final prefs = await SharedPreferences.getInstance();
-    final colorValue = prefs.getInt(_themeColorKey) ?? seedColor.toARGB32();
+    final colorValue = prefs.getInt(themeColorKey) ?? seedColor.toARGB32();
     return Color(colorValue);
   }
 
   Future<void> setThemeMode(AppThemeMode theme) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt(_themeModeKey, theme.index);
+    await prefs.setInt(themeModeKey, theme.index);
   }
   Future<AppThemeMode> getThemeMode() async {
     final prefs = await SharedPreferences.getInstance();
-    final themeIndex = prefs.getInt(_themeModeKey) ?? AppThemeMode.system.index;
+    final themeIndex = prefs.getInt(themeModeKey) ?? AppThemeMode.system.index;
     return AppThemeMode.values[themeIndex];
   }
 }
