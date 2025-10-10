cae532c6b7a3dbe7b75d3d67e787dc9700bb726d
J-shw <91017208+J-shw@users.noreply.github.com>
2025-10-10 11:01:52 -0600

    Added a isSelcted mode

--- app/lib/widgets/pills/pill_circle.dart
+++ app/lib/widgets/pills/pill_circle.dart
@@ -4,15 +4,33 @@ import 'package:menstrudel/models/pills/pill_status_enum.dart';
 class PillCircle extends StatelessWidget {
   final int dayNumber;
   final PillVisualStatus status;
+  final bool isSelected;
 
-  const PillCircle({super.key, required this.dayNumber, required this.status});
+  const PillCircle({
+    super.key, 
+    required this.dayNumber, 
+    required this.status,
+    this.isSelected = false,
+  });
 
   @override
   Widget build(BuildContext context) {
     final visuals = status.getVisuals(context, dayNumber);
+    final Color selectionColor = Theme.of(context).colorScheme.tertiary; 
+
+    BoxDecoration finalDecoration = visuals.decoration;
+    
+    if (isSelected) {
+      finalDecoration = finalDecoration.copyWith(
+        border: Border.all(
+          color: selectionColor, 
+          width: 3,
+        )
+      );
+    }
 
     return Container(
-      decoration: visuals.decoration,
+      decoration: finalDecoration,
       child: Center(child: visuals.child),
     );
   }
