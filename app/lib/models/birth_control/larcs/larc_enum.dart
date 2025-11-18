171e69dfeb4b281b1c62ab55b6c1dc112a0faf69
J-shw <91017208+J-shw@users.noreply.github.com>
2025-11-18 12:48:47 +0000

    Delete larc_enum.dart

--- app/lib/models/birth_control/larcs/larc_enum.dart
+++
@@ -1,9 +0,0 @@
-/// An enum of Long-Acting Reversible Contraceptives (LARCs) types.
-/// Currently only injection is supported, but more may be added in the future.
-enum LarcTypes {
-  iud,
-  implant,
-  injection,
-  ring,
-  patch,
-}
