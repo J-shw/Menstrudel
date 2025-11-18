b60ba753a3b349b53d262e8f6b34b7b7d88b1a7b
J-shw <91017208+J-shw@users.noreply.github.com>
2025-11-18 12:48:43 +0000

    Added new larc settings

---
+++ app/lib/models/birth_control/larcs/larc_types_enum.dart
@@ -0,0 +1,9 @@
+/// An enum of Long-Acting Reversible Contraceptives (LARCs) types.
+/// Currently only injection is supported, but more may be added in the future.
+enum LarcTypes {
+  iud,
+  implant,
+  injection,
+  ring,
+  patch,
+}
