65af4b5b869f8f87c42d0d52b77683bbf296b3f8
J-shw <91017208+J-shw@users.noreply.github.com>
2025-11-18 14:23:11 +0000

    Added extension to provide translations

--- app/lib/models/birth_control/larcs/larc_types_enum.dart
+++ app/lib/models/birth_control/larcs/larc_types_enum.dart
@@ -1,9 +1,29 @@
+import 'package:menstrudel/l10n/app_localizations.dart';
+
 /// An enum of Long-Acting Reversible Contraceptives (LARCs) types.
-/// Currently only injection is supported, but more may be added in the future.
+/// Currently only injection is supported, but more may be added in the future, so I have added some as placeholders.
 enum LarcTypes {
   iud,
   implant,
   injection,
   ring,
   patch,
+}
+
+/// Extension to get localized display names for LarcTypes.
+extension LarcTypeDisplay on LarcTypes {
+  String getDisplayName(AppLocalizations l10n) {
+    switch (this) { 
+      case LarcTypes.iud:
+        return l10n.larcType_iud; 
+      case LarcTypes.implant:
+        return l10n.larcType_implant;
+      case LarcTypes.injection:
+        return l10n.larcType_injection;
+      case LarcTypes.ring:
+        return l10n.larcType_ring;
+      case LarcTypes.patch:
+        return l10n.larcType_patch;
+    }
+  }
 }
