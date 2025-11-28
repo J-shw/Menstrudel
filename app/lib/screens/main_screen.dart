aefe8079b0f40d1670bcd3c08cb9cca8d47d81ae
J-shw <91017208+J-shw@users.noreply.github.com>
2025-11-28 18:44:43 +0000

    Removed old tampon logic + added sanitary screen logic

--- app/lib/screens/main_screen.dart
+++ app/lib/screens/main_screen.dart
@@ -10,7 +10,6 @@ import 'package:menstrudel/l10n/app_localizations.dart';
 import 'package:menstrudel/services/settings_service.dart';
 import 'package:provider/provider.dart';
 import 'package:menstrudel/services/period_service.dart';
-import 'package:menstrudel/services/notification_service.dart';
 import 'package:menstrudel/screens/larc_screen.dart';
 
 enum FabState {
@@ -28,37 +27,11 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 1;
-  FabState _fabState = FabState.logPeriod;
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
-  }
-
-  Future<void> _handleTamponReminderCountdown(PeriodService service) async {
-    final dueDate = await NotificationService.getTamponReminderScheduledTime();
-
-    if (dueDate == null) {
-      if (mounted) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-              content:
-                  Text(AppLocalizations.of(context)!.logScreen_couldNotCancelReminder)),
-        );
-      }
-      return;
-    }
-
-    if (mounted) {
-      showDialog(
-        context: context,
-        builder: (context) => ReminderCountdownDialog(
-          dueDate: dueDate,
-          onDelete: () => service.handleCancelReminder(context),
-        ),
-      );
-    }
   }
 
   Widget _buildLogPeriodFab(BuildContext context) {
@@ -72,54 +45,6 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 
-  Widget _buildSetReminderFab(BuildContext context) {
-    final l10n = AppLocalizations.of(context)!;
-    return FloatingActionButton(
-      key: const ValueKey('set_reminder_fab'),
-      tooltip: l10n.mainScreen_tooltipSetReminder,
-      onPressed: () => context.read<PeriodService>().handleTamponReminder(context),
-      child: const Icon(Icons.add_alarm),
-    );
-  }
-
-  Widget _buildCountDownReminderFab(BuildContext context) {
-    final l10n = AppLocalizations.of(context)!;
-    final service = context.read<PeriodService>();
-    return FloatingActionButton(
-      key: const ValueKey('countdown_reminder_fab'),
-      tooltip: l10n.mainScreen_tooltipCancelReminder,
-      onPressed: () => _handleTamponReminderCountdown(service),
-      child: const Icon(Icons.timer),
-    );
-  }
-
-  Widget _buildFab(BuildContext context, bool isReminderButtonAlwaysVisible) {
-    if (isReminderButtonAlwaysVisible) {
-      final Widget reminderFab = _fabState == FabState.cancelReminder
-          ? _buildCountDownReminderFab(context)
-          : _buildSetReminderFab(context);
-
-      return Column(
-        key: const ValueKey('multi_fab'),
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          reminderFab,
-          const SizedBox(height: 16),
-          _buildLogPeriodFab(context),
-        ],
-      );
-    }
-
-    switch (_fabState) {
-      case FabState.setReminder:
-        return _buildSetReminderFab(context);
-      case FabState.cancelReminder:
-        return _buildCountDownReminderFab(context);
-      case FabState.logPeriod:
-        return _buildLogPeriodFab(context);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context)!;
@@ -127,36 +52,13 @@ class _MainScreenState extends State<MainScreen> {
 
     final bool isPillNavEnabled = settingsService.isPillNavEnabled;
     final bool isLarcNavEnabled = settingsService.isLarcNavEnabled;
-    final bool isReminderButtonAlwaysVisible =
-        settingsService.areAlwaysShowReminderButtonEnabled;
+    final bool isSanitaryNavEnabled = settingsService.isSanitaryNavEnabled;
       
     /// Define pages on enabled features
     final List<Widget> pages = <Widget>[
       const InsightsScreen(),
-      LogsScreen(
-        onFabStateChange: (FabState suggestedState) {
-          FabState finalState;
-          if (isReminderButtonAlwaysVisible) {
-            finalState = (suggestedState == FabState.cancelReminder)
-                ? FabState.cancelReminder
-                : FabState.setReminder;
-          } else {
-            finalState = suggestedState;
-          }
-
-          if (_fabState != finalState) {
-            WidgetsBinding.instance.addPostFrameCallback((_) {
-              if (mounted) {
-                setState(() {
-                  _fabState = finalState;
-                });
-              }
-            });
-          }
-        },
-        isReminderButtonAlwaysVisible: isReminderButtonAlwaysVisible,
-      ),
-      const SanitaryScreen(),
+      const LogsScreen(),
+      if (isSanitaryNavEnabled) const SanitaryScreen(),
       if (isPillNavEnabled) const PillsScreen(),
       if (isLarcNavEnabled) const LarcScreen(),
       const SettingsScreen(),
@@ -195,7 +97,7 @@ class _MainScreenState extends State<MainScreen> {
                   child: child,
                 );
               },
-              child: _buildFab(context, isReminderButtonAlwaysVisible),
+              child: _buildLogPeriodFab(context),
             )
           : null,
     );
