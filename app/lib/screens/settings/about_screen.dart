1b59859481a2596ae44d6a8186b031cf107b69b8
J-shw <91017208+J-shw@users.noreply.github.com>
2025-12-07 11:21:52 +0000

    Added discord link

--- app/lib/screens/settings/about_screen.dart
+++ app/lib/screens/settings/about_screen.dart
@@ -90,6 +90,18 @@ class _AboutScreenState extends State<AboutScreen> {
                 trailing: const Icon(Icons.chevron_right),
                 onTap: _shareWebsite,
               ),
+
+              const Divider(height: 1, indent: 16, endIndent: 16),
+
+              ListTile(
+                leading: const Icon(Icons.code_rounded),
+                title: Text(l10n.aboutScreen_github),
+                subtitle: Text(l10n.aboutScreen_githubSubtitle),
+                trailing: const Icon(Icons.chevron_right),
+                onTap: () {
+                  _launchUrl('https://discord.gg/jWrH9DvACS', l10n);
+                },
+              ),
             ],
           ),
         ),
