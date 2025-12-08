e282e0733038086c7b9d45041b96c72c00f6dcbc
J-shw <91017208+J-shw@users.noreply.github.com>
2025-12-08 18:27:04 +0000

    Added logging reminder set if flow is recorded

--- app/lib/services/period_logger_service.dart
+++ app/lib/services/period_logger_service.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:menstrudel/l10n/app_localizations.dart';
 import 'package:menstrudel/models/flows/flow_enum.dart';
 import 'package:menstrudel/widgets/sheets/symptom_entry_sheet.dart';
 import 'package:menstrudel/models/period_logs/log_day.dart';
 import 'package:menstrudel/database/repositories/periods_repository.dart';
+import 'package:menstrudel/services/notification_service.dart';
+import 'package:menstrudel/services/settings_service.dart';
 import 'package:menstrudel/utils/exceptions.dart';
+import 'package:provider/provider.dart';
 
 class PeriodLoggerService {
   static Future<bool> showAndLogPeriod(BuildContext context, DateTime selectedDate) async {
@@ -36,6 +40,29 @@ class PeriodLoggerService {
 
       try {
         await periodsRepo.createPeriodLog(newEntry);
+        if (newEntry.flow != FlowRate.none && context.mounted) {
+          final settingsService = context.read<SettingsService>();
+          final nextDay = newEntry.date.add(const Duration(days: 1));
+          final reminderTime = settingsService.loggingReminderTime;
+          final bool isReminderEnabled = settingsService.isLoggingReminderNotificationEnabled;
+          final l10n = AppLocalizations.of(context)!;
+
+          final scheduledTime = DateTime(
+            nextDay.year,
+            nextDay.month,
+            nextDay.day,
+            reminderTime.hour,
+            reminderTime.minute,
+          );
+
+          NotificationService.scheduleLoggingReminder(
+            scheduledTime: scheduledTime,
+            isEnabled: isReminderEnabled,
+            title: l10n.settingsScreen_loggingReminderTitle,
+            body: l10n.settingsScreen_loggingReminderBody,
+          );
+          
+        }
       } on DuplicateLogException catch (e) {
         if (!context.mounted) return false;
         ScaffoldMessenger.of(context).showSnackBar(
