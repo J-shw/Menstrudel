8c813e8066e3a1e9cfcb20284f0e5c729bc9f229
J-shw <91017208+J-shw@users.noreply.github.com>
2026-01-05 22:12:16 +0000

    Allowed nullable values + updated date name to dateTime

--- app/lib/models/sex/sex_log_entry.dart
+++ app/lib/models/sex/sex_log_entry.dart
@@ -5,38 +5,38 @@ import 'package:menstrudel/models/sex/sex_type_enum.dart';
 
 class SexLogEntry {
   int? id;
-  final DateTime date;
-  final SexTypes sexType;
-  final SexParticipationTypes participationType;
-  final SexProtectionTypes protectionType;
-  final bool protectionUsed;
+  final DateTime dateTime;
+  final SexTypes? sexType;
+  final SexParticipationTypes? participationType;
+  final SexProtectionTypes? protectionType;
+  final bool? protectionUsed;
   final String? note;
 
   SexLogEntry({
     this.id,
-    required this.date,
-    required this.sexType,
-    required this.participationType,
-    required this.protectionType,
-    required this.protectionUsed,
+    required this.dateTime,
+    this.sexType,
+    this.participationType,
+    this.protectionType,
+    this.protectionUsed,
     this.note,
   });
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
-      'date': date.toIso8601String(),
-      'sexType': sexType.dbName,
-      'participationType': participationType.dbName,
-      'protectionType': protectionType.dbName,
-      'protectionUsed': protectionUsed ? 1 : 0,
+      'date_time': dateTime.toIso8601String(),
+      'sex_type': sexType?.dbName,
+      'participation_type': participationType?.dbName,
+      'protection_type': protectionType?.dbName,
+      'protection_used': protectionUsed == null ? null : (protectionUsed! ? 1 : 0),
       'note': note,
     };
   }
 
   SexLogEntry copyWith({
 		int? id,
-		DateTime? date,
+		DateTime? dateTime,
     SexTypes? sexType,
     SexParticipationTypes? participationType,
     SexProtectionTypes? protectionType,
@@ -45,7 +45,7 @@ class SexLogEntry {
 	}) {
 		return SexLogEntry(
 			id: id ?? this.id,
-			date: date ?? this.date,
+			dateTime: dateTime ?? this.dateTime,
       sexType: sexType ?? this.sexType,
       participationType: participationType ?? this.participationType,
       protectionType: protectionType ?? this.protectionType,
@@ -57,11 +57,11 @@ class SexLogEntry {
   static SexLogEntry fromMap(Map<String, dynamic> map) {
     return SexLogEntry(
       id: map['id'] as int,
-      date: DateTime.parse(map['date'] as String),
-      sexType: SexTypes.fromDbName(map['sexType'] as String),
-      participationType: SexParticipationTypes.fromDbName(map['participationType'] as String),
-      protectionType: SexProtectionTypes.fromDbName(map['protectionType'] as String),
-      protectionUsed: (map['protectionUsed'] as int) == 1,
+      dateTime: DateTime.parse(map['date_time'] as String),
+      sexType: map['sex_type'] != null ? SexTypes.fromDbName(map['sex_type']) : null,
+      participationType: map['participation_type'] != null ? SexParticipationTypes.fromDbName(map['participation_type'] as String) : null,
+      protectionType: map['protection_type'] != null ? SexProtectionTypes.fromDbName(map['protection_type'] as String) : null,
+      protectionUsed: map['protection_used'] != null ? (map['protection_used'] as int) == 1 : null,
       note: map['note'],
     );
   }
