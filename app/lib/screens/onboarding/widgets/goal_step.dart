70c80b70b00e85377694e07a617cb395c053d02c
J-shw <91017208+J-shw@users.noreply.github.com>
2026-01-13 23:26:43 +0000

    Uses new global goal widget card

--- app/lib/screens/onboarding/widgets/goal_step.dart
+++ app/lib/screens/onboarding/widgets/goal_step.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:menstrudel/l10n/app_localizations.dart';
 import 'package:menstrudel/models/app/user_goal_types_enum.dart';
+import 'package:menstrudel/widgets/goal_card.dart';
 
 class GoalStep extends StatelessWidget {
   final UserGoalTypes selectedGoal;
@@ -48,7 +49,7 @@ class GoalStep extends StatelessWidget {
             childAspectRatio: 1.1,
             children: UserGoalTypes.values.map((goal) {
               final isSelected = goal == selectedGoal;
-              return _GoalCard(
+              return GoalCard(
                 title: goal.getDisplayName(l10n),
                 icon: goal.icon,
                 isSelected: isSelected,
@@ -57,61 +58,6 @@ class GoalStep extends StatelessWidget {
             }).toList(),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class _GoalCard extends StatelessWidget {
-  final String title;
-  final IconData icon;
-  final bool isSelected;
-  final VoidCallback onTap;
-
-  const _GoalCard({
-    required this.title,
-    required this.icon,
-    required this.isSelected,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
-
-    return InkWell(
-      onTap: onTap,
-      borderRadius: BorderRadius.circular(20),
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 200),
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: isSelected ? colorScheme.primaryContainer : colorScheme.surface,
-          borderRadius: BorderRadius.circular(20),
-          border: Border.all(
-            color: isSelected ? colorScheme.primary : colorScheme.outlineVariant,
-            width: isSelected ? 2 : 1,
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              icon,
-              size: 32,
-              color: isSelected ? colorScheme.primary : colorScheme.onSurfaceVariant,
-            ),
-            const SizedBox(height: 12),
-            Text(
-              title,
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                    fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
-                    color: isSelected ? colorScheme.onPrimaryContainer : colorScheme.onSurface,
-                  ),
-            ),
-          ],
-        ),
       ),
     );
   }
