726b6fd0d15d6688ffd300f6621ec73ee9dc2385
J-shw <91017208+J-shw@users.noreply.github.com>
2026-01-17 19:31:14 +0000

    Create logs_screen.dart

---
+++ app/lib/screens/dashboards/logs/logs_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:menstrudel/screens/dashboards/logs/widgets/logs_screen_insights_tab.dart';
+import 'package:menstrudel/screens/dashboards/logs/widgets/logs_screen_log_tab.dart';
+import 'package:menstrudel/screens/dashboards/logs/widgets/logs_screen_period_quick_view_tab.dart';
+import 'package:menstrudel/l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:menstrudel/services/period_service.dart';
+
+class LogsScreen extends StatefulWidget {
+  const LogsScreen({super.key});
+
+  @override
+  State<LogsScreen> createState() => LogsScreenState();
+}
+
+class LogsScreenState extends State<LogsScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+    final colorScheme = Theme.of(context).colorScheme;
+    final periodService = context.watch<PeriodService>();
+
+    return DefaultTabController(
+      length: 3,
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              TabBar(
+                labelColor: colorScheme.primary,
+                unselectedLabelColor: colorScheme.onSurfaceVariant,
+                indicatorSize: TabBarIndicatorSize.tab,
+                tabs: [
+                  Tab(text: "Main"), //TODO: Needs a new name and needs to be localised. Not sure what to call it yet...
+                  Tab(text: l10n.logs),
+                  Tab(text: l10n.insights),
+                ],
+              ),
+
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    LogsScreenPeriodQuickViewTab(periodService: periodService),
+                    LogsScreenLogTab(periodService: periodService),
+                    LogsScreenInsightsTab(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
