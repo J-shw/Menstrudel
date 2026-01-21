2f90d08ae0c6007c2d98b332316d0a2e823be723
J-shw <91017208+J-shw@users.noreply.github.com>
2026-01-22 05:52:53 +0800

    updated name + added new iud options

--- app/lib/models/birth_control/larcs/larc_types_enum.dart
+++ app/lib/models/birth_control/larcs/larc_types_enum.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:menstrudel/l10n/app_localizations.dart';
 
-/// An enum of Long-Acting Reversible Contraceptives (LARCs) types.
-enum LarcTypes {
-  iud,
+/// Defines contraceptive methods that are duration-based and reversible.
+/// 
+/// Daily contraceptive pills are excluded from this enum and handled by a separate system
+enum ReversibleContraceptiveTypes {
+  hormonalIud,
+  copperIud,
   implant,
   injection,
   ring,
@@ -12,33 +15,36 @@ enum LarcTypes {
   /// Gets the localised display name
   String getDisplayName(AppLocalizations l10n) {
     return switch (this) { 
-      LarcTypes.iud => l10n.larcType_iud,
-      LarcTypes.implant => l10n.larcType_implant,
-      LarcTypes.injection => l10n.larcType_injection,
-      LarcTypes.ring => l10n.larcType_ring,
-      LarcTypes.patch => l10n.larcType_patch,
+      ReversibleContraceptiveTypes.hormonalIud => l10n.reversibleContraceptive_hormonalIud,
+      ReversibleContraceptiveTypes.copperIud => l10n.reversibleContraceptive_copperIud,
+      ReversibleContraceptiveTypes.implant => l10n.reversibleContraceptive_implant,
+      ReversibleContraceptiveTypes.injection => l10n.reversibleContraceptive_injection,
+      ReversibleContraceptiveTypes.ring => l10n.reversibleContraceptive_ring,
+      ReversibleContraceptiveTypes.patch => l10n.reversibleContraceptive_patch,
     };
   }
 
   /// gets the associated icon
   IconData getIcon(){
     return switch (this) { 
-      LarcTypes.iud => Icons.device_thermostat_rounded,
-      LarcTypes.implant => Icons.fiber_manual_record_outlined,
-      LarcTypes.injection => Icons.medical_services_outlined,
-      LarcTypes.ring => Icons.trip_origin_rounded,
-      LarcTypes.patch => Icons.square_outlined,
+      ReversibleContraceptiveTypes.hormonalIud => Icons.device_thermostat_rounded,
+      ReversibleContraceptiveTypes.copperIud => Icons.device_thermostat_rounded,
+      ReversibleContraceptiveTypes.implant => Icons.horizontal_rule_rounded,
+      ReversibleContraceptiveTypes.injection => Icons.vaccines_rounded,
+      ReversibleContraceptiveTypes.ring => Icons.trip_origin_rounded,
+      ReversibleContraceptiveTypes.patch => Icons.layers_rounded,
     };
   }
 
-  /// The default duration in days for each LARC type
+  /// The default duration in days for each contraception type
   int get defaultDurationDays {
     return switch (this) {
-      LarcTypes.iud => 1825,
-      LarcTypes.implant => 1095,
-      LarcTypes.injection => 84,
-      LarcTypes.ring => 21,
-      LarcTypes.patch => 28,
+      ReversibleContraceptiveTypes.hormonalIud => 1825,
+      ReversibleContraceptiveTypes.copperIud => 3650,
+      ReversibleContraceptiveTypes.implant => 1095,
+      ReversibleContraceptiveTypes.injection => 84,
+      ReversibleContraceptiveTypes.ring => 21,
+      ReversibleContraceptiveTypes.patch => 28,
     };
   }
 }
