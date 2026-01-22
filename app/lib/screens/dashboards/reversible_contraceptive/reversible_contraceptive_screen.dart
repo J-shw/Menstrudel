45d54cd5a90d6425e208f9276cbecc738e80ae98
J-shw <91017208+J-shw@users.noreply.github.com>
2026-01-22 12:21:32 +0800

    Updated import

--- app/lib/screens/dashboards/reversible_contraceptive/reversible_contraceptive_screen.dart
+++ app/lib/screens/dashboards/reversible_contraceptive/reversible_contraceptive_screen.dart
@@ -8,7 +8,7 @@ import 'package:menstrudel/l10n/app_localizations.dart';
 import 'package:menstrudel/models/birth_control/reversible_contraceptives/reversible_contraceptive_log_entry.dart';
 import 'package:menstrudel/services/notification_service.dart';
 import 'package:menstrudel/services/settings_service.dart';
-import 'package:menstrudel/controllers/log_larc_ui_controller.dart';
+import 'package:menstrudel/controllers/log_reversible_contraceptive_ui_controller.dart';
 import 'package:timezone/timezone.dart' as tz;
 
 class ReversibleContraceptiveScreen extends StatefulWidget {
