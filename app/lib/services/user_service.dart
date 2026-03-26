7b336c902aa17c64149be0294d54fd70f4f84cde
J-shw <91017208+J-shw@users.noreply.github.com>
2026-03-26 12:58:10 +0100

    Added new set methods for easy configuration

--- app/lib/services/user_service.dart
+++ app/lib/services/user_service.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:menstrudel/database/repositories/user_repository.dart';
 import 'package:menstrudel/models/app/user_entry.dart';
 import 'package:age_calculator/age_calculator.dart';
+import 'package:menstrudel/models/app/user_goal_types_enum.dart';
+import 'package:menstrudel/services/settings_service.dart';
 
 /// This app does not have users in the sense of logins and cloud. 
 /// In this case, a user is just the user of the app. 
@@ -42,4 +44,45 @@ class UserService extends ChangeNotifier {
     _isLoading = false;    
     notifyListeners();
   }
+
+  /// Updates the complete user.
+  Future<void> updateUser(UserEntry user) async {
+    _user = user;
+    notifyListeners();
+    _userRepo.updateUser(user);
+  }
+
+  /// Updates the user's date of birth.
+  Future<void> setBirthDate(DateTime date) async {
+    if (_user == null) return;
+    _user = _user!.copyWith(birthDate: date);
+    notifyListeners();
+    _userRepo.updateUser(_user!);
+  }
+
+  /// Remove date of birth.
+  Future<void> removeBirthDate() async {
+    if (_user == null) return;
+    _user = _user!.copyWith(birthDate: null);
+    notifyListeners();
+    _userRepo.updateUser(_user!);
+  }
+
+  /// Updates the user's name
+  Future<void> setName(String name) async {
+    if (_user == null) return;
+    _user = _user!.copyWith(name: name);
+    notifyListeners();
+    _userRepo.updateUser(_user!);
+  }
+
+  /// Updates the user's primary goal
+  Future<void> setPrimaryGoal(UserGoalTypes goal, SettingsService settingsService) async {
+    if (_user == null) return;
+    _user = _user!.copyWith(primaryGoal: goal);
+    notifyListeners();
+    _userRepo.updateUser(_user!);
+    settingsService.applySettingsForGoal(goal);
+  }
+
 }
