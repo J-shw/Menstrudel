23c0e2200df136464c211c41fce4c10f6d4f1a59
J-shw <91017208+J-shw@users.noreply.github.com>
2026-03-26 12:28:35 +0100

    Added age

--- app/lib/services/user_service.dart
+++ app/lib/services/user_service.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:menstrudel/database/repositories/user_repository.dart';
 import 'package:menstrudel/models/app/user_entry.dart';
+import 'package:age_calculator/age_calculator.dart';
 
 /// This app does not have users in the sense of logins and cloud. 
 /// In this case, a user is just the user of the app. 
-/// This serivce just lets the app get date of birth, name etc.
+/// This serivce just lets the app get date of birth, name, age etc.
 class UserService extends ChangeNotifier {
   final UserRepository _userRepo;
   
@@ -14,11 +15,14 @@ class UserService extends ChangeNotifier {
 
   UserEntry? _user;
   bool _isLoading = false;
+  int _age = 0;
 
   /// The user data.
   UserEntry? get user => _user;
   /// Whether service is loading
   bool get isLoading => _isLoading;
+  /// The users age. If DoB is provided.
+  int get age => _age;
 
   /// Loads user data.
   Future<void> loadUser() async {
@@ -30,6 +34,10 @@ class UserService extends ChangeNotifier {
     notifyListeners();
 
     _user = await _userRepo.getUser();
+
+    if (_user != null && _user!.birthDate != null) {
+      _age  = AgeCalculator.age(_user!.birthDate!).years;
+    }
 
     _isLoading = false;    
     notifyListeners();
